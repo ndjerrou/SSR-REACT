d64816e76f696d94abbca59142003487fa3eda8e
ndjerrou <modernejs@gmail.com>
2019-09-27 13:30:45 +0100

    SSR bundle file generated

---
+++ src/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const React = require('react');
+const { renderToString } = require('react-dom/server');
+const Home = require('../src/client/components/Home').default;
+const app = express();
+
+app.get('/', (req, res) => {
+  const content = renderToString(<Home />);
+  res.send(content);
+});
+
+app.listen(3000, () => {
+  console.log('Listening on port 3000');
+});
---
+++ webpack.server.js
@@ -0,0 +1,31 @@
+const path = require('path');
+module.exports = {
+  //inform webpack that we want a bundle for nodejs rather than for the browser
+  target: 'node',
+
+  //tell webpack the root file of our application
+  entry: './src/index.js',
+  // tell webpack where to put the outputfile that is generated
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'build')
+  },
+
+  // tell webpack : run babel on evevry file it runs through
+  module: {
+    rules: [
+      {
+        test: /\.js?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        options: {
+          presets: [
+            'react',
+            'stage-0',
+            ['env', { targets: { browsers: ['last 2 versions'] } }]
+          ]
+        }
+      }
+    ]
+  }
+};
